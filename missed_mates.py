aaa926a9aed800b2f6166f04590757c0b2aa0ea9
Andrew Lamoureux <andrew@vector35.com>
2016-09-21 23:10:25 -0400

    missed mates stuff

---
+++ missed_mates.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+
+import chess
+import chess.pgn
+import chess.uci
+
+assert(sys.argv[1:])
+pgn = open(sys.argv[1])
+
+engine = chess.uci.popen_engine("stockfish")
+engine.uci()
+
+gameNum=1
+while 1:
+	game = chess.pgn.read_game(pgn)
+	if not game: break
+
+	node = game
+	while not node.is_end():
+		print "after move: %s" % node.move
+		print node.board()
+
+		engine.position(node.board())
+		(moveBest, movePonder) = engine.go(movetime=3000)
+		print moveBest
+		print dir(moveBest)
+
+		node = node.variation(0)
+
+	gameNum += 1
+	break
